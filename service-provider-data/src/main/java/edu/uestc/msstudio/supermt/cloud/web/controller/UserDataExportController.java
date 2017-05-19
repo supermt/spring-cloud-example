e4f3146210fd368694ad4264af77587b2af9be2f
Jinghuan, Yu <jinghuan.yu@sap.com>
2017-05-19 17:48:43 +0800

    Add Common Models

--- service-provider-data/src/main/java/edu/uestc/msstudio/supermt/cloud/web/controller/UserDataExportController.java
+++ service-provider-data/src/main/java/edu/uestc/msstudio/supermt/cloud/web/controller/UserDataExportController.java
@@ -1,6 +1,17 @@
 package edu.uestc.msstudio.supermt.cloud.web.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
 public class UserDataExportController
 {
-
+    @GetMapping("/{id}")
+    public HttpEntity<?> getUserByID(@PathVariable Long id)
+    {
+        
+        return null;
+    }
 }
